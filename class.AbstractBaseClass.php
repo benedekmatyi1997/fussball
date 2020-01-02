8cc31ca7de69648882b34f2e2371e96b2fb445a7
csabinho <csabi.burus@gmail.com>
2020-01-02 16:12:14 +0100

    Added getAsArray, initDB and getJoinArray

--- class.AbstractBaseClass.php
+++ class.AbstractBaseClass.php
@@ -1,8 +1,10 @@
 <?php
-
+require_once("class.db.php");
 abstract class AbstractBaseClass {
     protected static $columns=array();
     protected static $tablename;
+    protected static $all_elements;
+    protected static $db;
     
     abstract public function load($id);
     abstract public function update();
@@ -11,7 +13,7 @@ abstract class AbstractBaseClass {
     {
         if(!count(static::$columns))
         {
-            echo("Du hast vergessen das in der Klasse zu ändern!");
+            throw new Exception("Du hast vergessen das in der Klasse zu &auml;ndern!");
         }
         if(!strlen($prefix))
         {
@@ -24,6 +26,42 @@ abstract class AbstractBaseClass {
         }
         return $columns_with_prefix;
     }
-    
-//put your code here
+    public function getAsArray($prefix="")
+    {
+        if(!count(static::$columns))
+        {
+            throw new Exception("Du hast vergessen das in der Klasse zu &auml;ndern!");
+        }
+        $content_array=array();
+        foreach(static::$columns as $column)
+        {
+            $method_name="get".ucfirst($column);
+            if($this->$method_name() instanceof AbstractBaseClass)
+            {
+                $content_array=array_merge($content_array, $this->$method_name()->getAsArray($prefix.$column));
+            }
+            else
+            {
+                $content_array[$prefix.$column]=$this->$method_name();
+            }
+        }
+        
+        return $content_array;
+    }
+    protected static function initDB()
+    {
+        if(static::$db==null)
+        {
+            static::$db=DB::getDB();
+        }
+    }
+    protected static function getJoinArray(PDOStatement $stmt,array $joincolumns)
+    {
+        $joinarray=array();
+        for($i=1;$i<=count($joincolumns);$i++)
+        {
+            $stmt->bindColumn($i,$joinarray[$joincolumns[$i-1]]);
+        }
+        return $joinarray;
+    }    
 }
